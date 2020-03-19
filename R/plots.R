bfe9a206928357c59553de6a5d0eaffb2d9d0bf6
Luka Krajnc <luka.krajnc@gozdis.si>
2020-03-19 18:28:30 +0100

    added plotting functions for all and for failures

--- R/plots.R
+++ R/plots.R
@@ -35,9 +35,39 @@ plot_start_detection  <- function(dpa.list){
   }
 }
 
-plot_start_detection  <- function(dpa.list){
+plot_end_detection  <- function(dpa.list){
   for (i in 1:length(dpa.list)){
     print((dpa_detect_end(dpa.list[[i]], return.plot = T)))
+    ##Sys.sleep(2)
+    keyPressed = readkeygraph(paste0("[any key to continue, q to quit] file ",i,"/",length(dpa.list)))
+  }
+}
+
+plot_all  <- function(dpa.list){
+  for (i in 1:length(dpa.list)){
+    dpa  <- dpa.list[[i]]
+    print(plot(dpa$data$amplitude, type = "l",
+               xlab = paste0("Drilling depth [", dpa$footer$xUnit, "]"),
+               ylab= paste0("Resistograph density [", dpa$footer$yUnit, "]"),
+               main = paste0("Resistograph data: file ",dpa$footer$ID)))
+    ##Sys.sleep(2)
+    keyPressed = readkeygraph(paste0("[any key to continue, q to quit] file ",i,"/",length(dpa.list)))
+  }
+}
+
+
+plot_failures  <- function(dpa.trimmed){
+  failures  <-  separate_trim_failures(dpa.trimmed)
+  failures2  <- union(failures$failures.start, failures$failures.end)
+  names(failures2)  <-  union(names(failures$failures.start), names(failures$failures.end))
+
+  for (i in 1:length(failures2)){
+    dpa  <- failures2[[i]]
+    print(plot(dpa$amplitude, type = "l",
+               xlab = paste0("Drilling depth"),
+               ylab= paste0("Resistograph density"),
+               main = paste0("Resistograph data, trimming failure ",dpa$ID[1])))
+
     ##Sys.sleep(2)
     keyPressed = readkeygraph(paste0("[any key to continue, q to quit] file ",i,"/",length(dpa.list)))
   }

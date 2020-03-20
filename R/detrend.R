f90f98531edd8947e539967fbfcab44de5fb5e1b
Luka Krajnc <luka.krajnc@gozdis.si>
2020-03-20 15:42:52 +0100

    added GAM detrending, combined linear and GAM into one

--- R/detrend.R
+++ R/detrend.R
@@ -1,9 +1,17 @@
-dpa_detrend_linear <- function(dpa){
-  detrend <- lm(amplitude ~ position, data = dpa)
-  fit <- predict(detrend, newdata = dpa)
-  ## detrended  <- dpa$amplitude - fit + fit[1]
-  dpa <- dpa %>%
-    mutate(amplitude = amplitude - fit + fit[1])
-
+dpa_detrend <- function(dpa, type = c("linear","gam")){
+  if (type == "linear") {
+    trend <- lm(amplitude ~ position, data = dpa)
+    fit <- predict(trend, newdata = dpa)
+    dpa <- dpa %>%
+      dplyr::mutate(amplitude = amplitude - fit + fit[1])
+  } else if (type == "gam") {
+    if (requireNamespace("mgcv", quietly = TRUE)) {
+      m <- mgcv::gam(amplitude~s(position), data = dpa, method = "REML")
+      dpa <- dpa %>%
+        dplyr::mutate(amplitude =  amplitude - m$fitted.values + m$fitted.values[1])
+    } else {
+      stop("Package \"mgcv\" needed for GAM detrending. Please install it.")
+    }
+  }
   return(dpa)
 }

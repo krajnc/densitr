d09435bf6a48172c936ca12d0b7bc4cd54e1400d
Luka Krajnc <luka.krajnc@gozdis.si>
2020-03-20 14:57:22 +0100

    added class assignment

--- R/load.R
+++ R/load.R
@@ -13,36 +13,41 @@ extract_dpa_name  <- function(string){
 }
 
 ### rabiš tudi magitrr
-read_dpa_file <- function(dpa.file){
+read_dpa <- function(file){
+  ## check ending
+  if (!grepl("\\.dpa$", file))  {
+    stop("not a *.dpa file")
+  }
 
-  dpa.read  <- readr::read_lines(dpa.file,skip=3)
+  dpa.read  <- readr::read_lines(file,skip=3)
 
   dpa.data <- dpa.read  %>%
     head(n = -14) %>%
     dplyr::tibble(amplitude=.) %>%
     dplyr::mutate_at("amplitude", as.numeric) %>%
     tibble::rowid_to_column(var = "position") %>%
-    dplyr::mutate(ID=extract_dpa_name(dpa.file))
+    dplyr::mutate(ID=extract_dpa_name(file))
 
   dpa.footer <-  dpa.read %>%
     tail(n=13) %>%
     paste(collapse = "\n")  %>%
     readr::read_csv(.,col_names = c("footer")) %>%
     tidyr::separate(footer, into = c("name","value"),sep="=") %>%
-    dplyr::mutate(ID=extract_dpa_name(dpa.file)) %>%
+    dplyr::mutate(ID=extract_dpa_name(file)) %>%
     tidyr::pivot_wider(names_from = name, values_from = value)
 
   ## dpa.complete <- dpa.data %>%
   ##   dplyr::left_join(dpa.footer, by="ID") #%>%    mutate_at(c("ID","position"), as.factor)
 
-  dpa.complete  <- list("data" = dpa.data, "footer" = dpa.footer)
-
-  return(dpa.complete)
-
+  d  <- list("data" = dpa.data, "footer" = dpa.footer)
+  class(d) <- 'dpa'
+  return(d)
 }
 
+
 ## funnkcija za load dpa files, if za recursive, uporabi pbapply če obstaja
-load_dpa  <- function(dpa.file = NULL, dpa.directory = "", recursive = TRUE) {
+load_dpa  <- function(dpa.file = NULL, dpa.directory = "",
+                      recursive = TRUE, name = "file") {
   if (is.null(dpa.file)) {
     ## read the whole directory, possibly recursively
     if (dir.exists(dpa.directory)) {
@@ -50,16 +55,25 @@ load_dpa  <- function(dpa.file = NULL, dpa.directory = "", recursive = TRUE) {
       dpa.files <- file.path(dpa.directory, dpa.files)
       print(paste0("found ",length(dpa.files), " dpa files, loading:"))
       if (requireNamespace("pbapply", quietly = TRUE)) {
-        dpa.list <-  pbapply::pblapply(dpa.files,  read_dpa_file)
+        dpa.list <-  pbapply::pblapply(dpa.files, read_dpa)
       } else {
-        dpa.list <-  lapply(dpa.files,  read_dpa_file)
+        dpa.list <-  lapply(dpa.files,  read_dpa)
       }
-      ## if recursive, name them properly also using folders
-      names(dpa.list) <-  gsub("*.dpa$","",dpa.files)
+      if (name == "file") {
+        ## name only using file names
+        names(dpa.list) <-  extract_dpa_name(dpa.files)
+      } else if (name == "folder"){
+        ## if recursive, name them properly also using folders
+        names(dpa.list) <-  gsub("*.dpa$","",dpa.files)
+      }
 
-      ## alternative, only use dpa name
-      ## sapply(regmatches(dpa.files, regexec("[\\w-]+?(?=\\.)", dpa.files, perl = TRUE)), "[", 1)
-      return(dpa.list)
+      info  <- lapply(dpa.list,function(x) x$footer) %>%
+        bind_rows()
+
+      data  <- lapply(dpa.list,function(x) x$data)
+      d.list <- list("data" = data, "info" = info)
+      class(d.list) <- 'dpa.list'
+      return(d.list)
     } else {
       ## fail directory doesn't exist
       warning("given directory does not exist")
@@ -68,7 +82,8 @@ load_dpa  <- function(dpa.file = NULL, dpa.directory = "", recursive = TRUE) {
     ## read a single file
     ## check if file exists and is not a directory
     if (file_test("-f", dpa.file)) {
-      dpa  <- read_dpa_file(dpa.file)
+      dpa  <- read_dpa(dpa.file)
+      # class(dpa) <- 'dpa'
       return(dpa)
     } else {
       ## fail reading a single file

619038fec0879e7456c1d28ae2d211f7bee3747d
Luka Krajnc <luka.krajnc@gozdis.si>
2020-03-26 14:10:09 +0100

    Add global definitions to a separate file

---
+++ R/globals.R
@@ -0,0 +1,2 @@
+## declare some global variables so that R CMD stops complaining
+utils::globalVariables(c(".", "amplitude", "dpa.list", "detection.start", "detection.end", "ID",  "keyPressed", "keydown"))
